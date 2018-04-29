f7d05fa819f678837a1f803a37aa79b04b00751f
Sean O'Connor <seaneo85@gmail.com>
2018-04-29 00:34:09 -0400

    fix the slider alt tag to be more relevant

--- front-page.php
+++ front-page.php
@@ -6,29 +6,29 @@
 			<div class="" id="slider">
 				<div class="container full-width">
 				  <ul class="slides">
-					<img src="<?php echo get_template_directory_uri(); ?>/library/images/home-banner-V2.jpg" alt="child with English Cream golden retriever puppy" width="100%" />
+					<img src="<?php echo get_template_directory_uri(); ?>/library/images/home-banner-V2.jpg" alt="purchase white golden retriever call to action" width="100%" />
 					<a href="<?php echo get_site_url(); ?>/reserve-puppy">
-						
+
 						<span class="blue-back">
 							<h3>Puppies arriving 2018!<br>
 								<strong>Now taking reservations for 2018 spring litters</strong>
 							</h3>
 						</span>
-					
+
 					</a>
 				</div> <!-- End .flexslider -->
 			</div> <!-- End slider -->
 
 			<div class="container" id="content">
 
-				
+
 						<header class="article-header">
 
 									<h1 class="page-name" itemprop="headline"><?php the_title(); ?>
 									<span><?php echo do_shortcode('[types field="website-description" id="37"][/types]'); ?></span>
 									<img alt="" src="<?php bloginfo('template_directory'); ?>/library/images/red-paw.png"></h1>
 
-									
+
 
 
 						</header> <!-- end article header -->
@@ -69,7 +69,7 @@
 
 
 			</div> <!-- end #content -->
-			
+
 			<aside class="bottom">
 				<div class="container">
 				<div class="row-fluid">
@@ -77,11 +77,11 @@
 						<h2>
 							Testimonials
 						</h2>
-						
+
 						<?php echo do_shortcode('[wpv-view name="Featured Testimonials"]'); ?>
-						
+
 						<a class="btn" href="testimonials">View All Testimonials</a>
-							
+
 					</div>
 				</div>
 				</div>
