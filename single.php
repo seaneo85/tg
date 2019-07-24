a09909d2efede2382c291137764fbe8919e21482
Sean O'Connor <soconnor@allegheny.edu>
2019-07-23 23:56:57 -0400

    finish the updates for the News & Updates singular posts

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap clearfix">
 
-					<div id="main" class="twelvecol first clearfix" role="main">
+					<div id="main" class="eightcol first clearfix" role="main">
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -13,13 +13,29 @@
 								<header class="article-header">
 
 									<h1 class="entry-title single-title page-name" itemprop="headline"><?php the_title(); ?><img alt="" src="<?php echo get_template_directory_uri(); ?>/library/images/red-paw.png"></h1>
-
+									
+									
+									
 								</header> <!-- end article header -->
 
-								<section class="entry-content clearfix" itemprop="articleBody">
+								<section class="entry-content clearfix news-updates" itemprop="articleBody">
+									
+									<?php
+									if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+										echo '<p>' . get_the_post_thumbnail( $page->ID, 'large' ) . '</p>';
+									}
+									?>
+									
+									<span class="posted-on">
+										<time class="entry-date published updated" datetime="<?php the_date( 'Y-m-d' ); ?>">
+											<i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo get_the_date( 'F j, Y' ); ?>
+										</time>
+									</span>
+									
 									<?php the_content(); ?>
+								
 								</section> <!-- end article section -->
-
+								</article>
 								
 
 								<?php comments_template(); ?>
@@ -43,6 +59,7 @@
 						<?php endif; ?>
 
 					</div> <!-- end #main -->
+					<?php get_sidebar(); ?>
 
 				</div> <!-- end #inner-content -->
 
